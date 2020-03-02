5ef676aa030c8da52c612ac42413b496d49f7e77
theazat <sayanazat@gmail.com>
2020-03-02 22:43:32 +0300

    Created ui components

---
+++ app/src/main/java/com/example/newsapp/ui/activity/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.newsapp.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        // Finding the Navigation Controller
+        var navController = findNavController(R.id.fragNavHost)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        bottomNavView.setupWithNavController(navController)
+
+        // Setting Up ActionBar with Navigation Controller
+        var appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.breakingFragment,
+                R.id.businessFragment,
+                R.id.sporFragment,
+                R.id.magazineFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
